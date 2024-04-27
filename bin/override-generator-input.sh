5aa57341ee92a1916907a01e0f29935feb0073fa
Wark99 <vaidamark99@gmail.com>
2024-04-27 21:23:20 +0300

    use custom script to build

--- bin/override-generator-input.sh
+++ bin/override-generator-input.sh
@@ -2,18 +2,9 @@
 
 # Check if exactly one argument is provided
 if [ $# -ne 1 ]; then
-  echo "Skipping generator.inputfile update."
-  exit 0
+    filepath="./src/main/resources/api.yaml"
+  else
+    filepath="./src/main/resources/$1"
 fi
 
-sudo apt-get update
-sudo apt-get install -y xmledit
-
-# New filepath provided as the first argument
-new_filepath="./src/main/resources/$1"
-
-# Update the XML file
-xmledit --update "/project/properties/property[@name='generator.inputfile']/@value" --value "NEW_PATH/TO/FILE" pom.xml
-
-# Success message
-echo "Successfully updated generator.inputfile to: $new_filepath"
+mvn clean install package -Dgenerator_inputfile="$filepath"
