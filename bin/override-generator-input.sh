8378116e2f2cb2719b6d2e0a3ea4285965233383
Wark99 <vaidamark99@gmail.com>
2024-04-27 20:17:10 +0300

    Script for editing generator input

--- bin/override-generator-input.sh
+++ bin/override-generator-input.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "generatorInputSpec=$1"
+# Check if exactly one argument is provided
+if [ $# -ne 1 ]; then
+  echo "Error: Please provide exactly one argument: the new filepath for generator.inputfile."
+  exit 1
+fi
+
+# New filepath provided as the first argument
+new_filepath="$1"
+
+# Update the XML file using xmlstarlet
+xmlstarlet ed -i "//properties/generator.inputfile" --value "$new_filepath" ./pom.xml
+
+# Success message
+echo "Successfully updated generator.inputfile to: $new_filepath"
