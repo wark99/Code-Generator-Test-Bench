9c51228b70fb917b9a0b72b880fb9bcaac57aa90
Wark99 <vaidamark99@gmail.com>
2024-05-25 21:14:08 +0300

    Ai generated code

---
+++ src/main/java/gen/Models.java
@@ -0,0 +1,47 @@
+package gen;
+
+import java.util.List;
+
+public class HTTPValidationError {
+    private List<ValidationError> detail;
+
+    public List<ValidationError> getDetail() {
+        return detail;
+    }
+
+    public void setDetail(List<ValidationError> detail) {
+        this.detail = detail;
+    }
+}
+
+import java.util.List;
+
+public class ValidationError {
+    private List<Object> loc;
+    private String msg;
+    private String type;
+
+    public List<Object> getLoc() {
+        return loc;
+    }
+
+    public void setLoc(List<Object> loc) {
+        this.loc = loc;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
