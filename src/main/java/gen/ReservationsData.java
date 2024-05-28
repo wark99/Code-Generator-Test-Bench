d801784524f749e5265d82ab3e85ce3e21e7dbfa
Wark99 <vaidamark99@gmail.com>
2024-05-28 19:56:01 +0300

    Extract all in one

---
+++ src/main/java/gen/ReservationsData.java
@@ -0,0 +1,9 @@
+package gen;
+
+public class ReservationsData {
+    private Reservation[] arrivi;
+    private Reservation[] partenze;
+    private Reservation[] prenotazioni;
+
+    // Getters and setters
+}
