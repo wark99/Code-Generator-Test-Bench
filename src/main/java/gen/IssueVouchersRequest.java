a7404ec281c271c423c231423448dffb87354b12
Wark99 <vaidamark99@gmail.com>
2024-05-28 19:37:56 +0300

    Extract all in one

---
+++ src/main/java/gen/IssueVouchersRequest.java
@@ -0,0 +1,7 @@
+package gen;
+
+public class IssueVouchersRequest {
+    private int quantity;
+    private int faceValue;
+    // Getters and setters
+}
---
+++ src/main/java/gen/Product.java
@@ -0,0 +1,21 @@
+package gen;
+
+public class Product {
+    private String productCode;
+    private String name;
+    private String description;
+    private boolean isValueBased;
+    private boolean isOpenValue;
+    private boolean isStatusAvailable;
+    private boolean isMultipleUse;
+    private String currency;
+    private int amount;
+    private Integer quantity;
+    private String expiryScheme;
+    private String validFrom;
+    private String validUntil;
+    private String image;
+    private String termsAndConditions;
+    // Getters and setters
+}
+
