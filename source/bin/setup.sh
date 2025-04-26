fb39e3663f972d57394384ae3600747dc8327ee0
vaidam <vaidamark99@gmail.com>
2025-04-26 19:40:36 +0300

    Add source for openbanking-us.yaml

---
+++ source/bin/setup.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+api_error() {
+   echo ""
+   echo "Response: $1"
+   echo ""
+   echo "API call failed! Check {partnerId}, {partnerSecret} and {appKey} and make sure you are located in the US, UK or Canada."
+   enter_to_exit
+}
+
+enter_to_exit() {
+  read -p "Press [Enter] to exit..."
+  exit
+}
+
+if [ "$#" -lt 3 ] || [ "$#" -gt 4 ]; then
+    echo "Usage: ./setup.sh {partnerId} {partnerSecret} {appKey} [--no-interaction]"
+    enter_to_exit
+fi
+
+echo "Step 1 - Creating access token ..."
+token_response=$(curl -s --location --request POST 'https://api.finicity.com/aggregation/v2/partners/authentication' \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+--header 'Finicity-App-Key:'$3 \
+--data-raw '{ "partnerId": "'$1'", "partnerSecret": "'$2'" }')
+
+# {"token":"P09NAglkaBTyJrHoFGmL"}
+if [[ "$token_response" != *"token"* ]]; then 
+  api_error "$token_response"
+fi;
+
+token=$(echo $token_response | sed -E 's/\{"token":"(.*)"\}/\1/')
+echo "Access token: "$token
+echo ""
+
+echo "Step 2 - Creating test customer ..."
+customer_response=$(curl -s --location --request POST 'https://api.finicity.com/aggregation/v2/customers/testing' \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+--header 'Finicity-App-Key:'$3 \
+--header 'Finicity-App-Token:'$token \
+--data-raw '{"username": "customer_'$RANDOM$RANDOM'" , "email":"'$RANDOM'@domain.com"}')
+
+# {"id":"5026948632","username":"customerusername1","createdDate":"1649244189"}
+if [[ "$customer_response" != *"username"* ]]; then 
+  api_error "$customer_response"
+fi;
+
+customer_id=$(echo $customer_response | sed -E 's/\{"id":"([0-9]*).*/\1/')
+echo "Customer ID: "$customer_id
+echo ""
+
+if [ "$4" == "--no-interaction" ]; then
+  echo "Steps 3 and 4 - Skipping Connect, adding customer accounts ..."
+  add_accounts_response=$(curl -s --location --request POST 'https://api.finicity.com/aggregation/v1/customers/'$customer_id'/institutions/102105/accounts/addall' \
+  --header 'Content-Type: application/json' \
+  --header 'Accept: application/json' \
+  --header 'Finicity-App-Key:'$3 \
+  --header 'Finicity-App-Token:'$token \
+  --data-raw '{"credentials":[{"name":"Banking Userid","value":"profile_03","id":"102105001"},{"name":"Banking Password","value":"profile_03","id":"102105002"}]}')
+
+  # { "accounts": [...]}
+  if [[ "$add_accounts_response" != *"accounts"* ]]; then 
+    api_error "$add_accounts_response"
+  fi;
+  echo "Accounts added"
+  echo ""
+fi
+
+if [ "$4" != "--no-interaction" ]; then
+  echo "Step 3 - Generating connect URL ..."
+  connect_url_response=$(curl -s --location --request POST 'https://api.finicity.com/connect/v2/generate' \
+  --header 'Content-Type: application/json' \
+  --header 'Accept: application/json' \
+  --header 'Finicity-App-Key:'$3 \
+  --header 'Finicity-App-Token:'$token \
+  --data-raw '{ "partnerId": "'$1'", "customerId": "'$customer_id'" }')
+
+  # {"link":"https://..."}
+  link=$(echo $connect_url_response | sed -E 's/\{"link":"(.*)"\}/\1/')
+  echo "URL created"
+  echo ""
+
+  echo "Step 4 - Ctrl+click on the URL below, search for 'FinBank Profiles - A', then sign in with 'profile_03'/'profile_03' and add all available accounts"
+  echo ""
+  echo $link
+  echo ""
+  read -p 'After you see "Your submission was successful. Thank you!", press [Enter] to continue ...'
+  echo ""
+fi
+
+echo "Step 5 - Refreshing accounts ..."
+accounts_response=$(curl -s --location --request POST 'https://api.finicity.com/aggregation/v1/customers/'$customer_id'/accounts' \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+--header 'Finicity-App-Key:'$3 \
+--header 'Finicity-App-Token:'$token \
+--data-raw '{}')
+
+# { "accounts": [...]}
+echo "Accounts refreshed"
+
+echo ""
+echo "APP_KEY: "$3
+echo "CUSTOMER_ID: "$customer_id
+echo "PARTNER_ID: "$1
+echo "PARTNER_SECRET: "$2
+echo ""
+
+echo "To run the API client tests:"
+echo ""
+echo "git clone https://github.com/Mastercard/open-banking-us-openapi"
+echo "cd open-banking-us-openapi/tests"
+echo "mvn clean test -DpartnerId="$1" -DpartnerSecret="$2" -DappKey="$3" -DcustomerId="$customer_id""
+echo ""
+
+echo "To run the Postman Collection:"
+echo ""
+echo "git clone https://github.com/Mastercard/open-banking-us-postman"
+echo "cd open-banking-us-postman"
+echo "npx newman run openbanking-us.postman_collection.json --env-var partnerId="$1" --env-var partnerSecret="$2" --env-var appKey="$3" --env-var customerId="$customer_id" --folder 'All APIs'"
+echo ""
+
+enter_to_exit
---
+++ source/tests/src/test/java/com/mastercard/openbanking/client/api/AppRegistrationApiTest.java
@@ -0,0 +1,128 @@
+package com.mastercard.openbanking.client.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.mastercard.openbanking.client.ApiException;
+import com.mastercard.openbanking.client.model.App;
+import com.mastercard.openbanking.client.model.InstitutionResponse;
+import com.mastercard.openbanking.client.test.BaseTest;
+import com.mastercard.openbanking.client.test.ModelFactory;
+import com.mastercard.openbanking.client.test.utils.AccountUtils;
+
+class AppRegistrationApiTest extends BaseTest {
+
+    private final AppRegistrationApi api = new AppRegistrationApi(apiClient);
+
+    @Test
+    void registerAppTest() {
+        try {
+            var application = ModelFactory.newApplication();
+            var registeredApplication = api.registerApp(application);
+            assertNotNull(registeredApplication.getPreAppId());
+            assertEquals("P", registeredApplication.getStatus());
+        } catch (ApiException e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    void getAppRegistrationStatusTest() {
+        try {
+            var statuses = api.getAppRegistrationStatus(null, null, null, null, null, null, null, null);
+            assertNotNull(statuses.getNumberOfRecordsPerPage());
+            assertNotNull(statuses.getPageNumber());
+            assertNotNull(statuses.getTotalPages());
+            assertNotNull(statuses.getTotalRecords());
+        } catch (ApiException e) {
+            Assertions.fail(e);
+        }
+    }
+
+    @Test
+    void modifyAppRegistrationTest() {
+        try {
+            var application = ModelFactory.newApplication();
+            var preAppId = api.registerApp(application).getPreAppId();
+            application.setAppDescription("Updated description");
+            var updatedApplication = api.modifyAppRegistration(String.valueOf(preAppId), application);
+            assertEquals(preAppId, updatedApplication.getPreAppId());
+            assertEquals("P", updatedApplication.getStatus());
+        } catch (ApiException e) {
+            Assertions.fail(e);
+        }
+    }
+
+
+    @Test
+    void getApplicationsTest() throws ApiException {
+    	try {
+    		Integer start = 1;
+    		Integer limit = 25;
+    		String status = "A";
+    		var applicationResponse = api.getApplications(start, limit, null, null, null, status);
+    		assertTrue(applicationResponse.getFound()>0);
+    		assertTrue(applicationResponse.getDisplaying()>0);
+            assertFalse(applicationResponse.getApplications().isEmpty());
+    	}catch (Exception e) {
+    		Assertions.fail(e);
+		}
+    }
+
+    @Test
+    void getRegisteredInstitutionsTest() throws ApiException {
+    	try {
+    		String status = "A";
+    		var applicationResponse = api.getApplications(1, 50, null, null, null, status);
+    		Optional<String> applicationIds = applicationResponse.getApplications().stream().map(App::getApplicationId).filter(appId -> Objects.nonNull(appId)).findFirst();
+    		String applicationId = applicationIds.get();
+    		Integer start = 1;
+    		Integer limit = 25;
+    		InstitutionResponse institutionResponse = api.getRegisteredInstitutions(applicationId, start, limit, null);
+    		assertTrue(institutionResponse.getDisplaying()>0);
+    		assertTrue(institutionResponse.getFound()>0);
+    		assertTrue(institutionResponse.getMoreAvailable());
+    		assertFalse(institutionResponse.getInstitutions().isEmpty());
+    	}catch (Exception e) {
+    		Assertions.fail(e);
+    	}
+    }
+
+    @Test
+    void setCustomerAppIDTest() {
+        try {
+            api.setCustomerAppID(CUSTOMER_ID, "1234");
+        } catch (ApiException e) {
+            // {"code":10007,"message":"invalid application id"}
+            logApiException(e);
+            assertErrorCodeEquals(10007, e);
+            assertErrorMessageEquals("invalid application id", e);
+        }
+    }
+
+    @Test
+    void migrateInstitutionLoginAccountsTest() {
+        try {
+            var accountApi = new AccountsApi(apiClient);
+            var existingAccount = AccountUtils.getCustomerAccounts(accountApi, CUSTOMER_ID).get(0);
+            var existingInstitutionLoginId = existingAccount.getInstitutionLoginId();
+            // Enable this to actually migrate accounts
+            // var accounts = api.migrateInstitutionLoginAccounts(CUSTOMER_ID, String.valueOf(existingInstitutionLoginId));
+            // assertTrue(accounts.getAccounts().size() > 0);
+        } catch (ApiException e) {
+            Assertions.fail(e);
+        }
+    }
+
+
+
+}
